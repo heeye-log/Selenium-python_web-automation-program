9d92b79584f9247d81f1974c3e2b6e006cb49f1e
HYEYEON KIM <121289071+heeye-log@users.noreply.github.com>
2023-01-14 20:28:52 +0900

    Update comment.py

--- 자동댓글달기/comment.py
+++ 자동댓글달기/comment.py
@@ -14,7 +14,7 @@ while count < 5 :
     driver.find_element_by_name("name").send_keys("아무거나")
 
     driver.find_element_by_name("password").click()
-    driver.find_element_by_name("password").send_keys("아무거나")
+    driver.find_element_by_name("password").send_keys("123456")
 
     driver.find_element_by_css_selector("#entry0Comment > div.comments.guest > form > div > textarea").click()
     driver.find_element_by_xpath("//*[@id='entry0Comment']/div[2]/form/div/textarea").send_keys("댓글 입력 예제")
